df6dbb14307c90169711e366abdb4033283ede98
kion <kion@dashgl.com>
2017-10-19 13:42:08 +0000

    uploading base

---
+++ 01_Open_A_Window/main.c
@@ -0,0 +1,22 @@
+#include <gtk/gtk.h>
+
+int main(int argc, char *argv[]) {
+
+	GtkWidget *window;
+
+	gtk_init(&argc, &argv);
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window), "Invaders Tutorial");
+	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+	gtk_window_set_default_size(GTK_WINDOW(window), 640, 480);
+	gtk_window_set_type_hint(GTK_WINDOW(window), GDK_WINDOW_TYPE_HINT_UTILITY);
+	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+	gtk_widget_show_all(window);
+
+	gtk_main();
+
+	return 0;
+
+}
