8e8008f7cc27ffcb566489d782f3f505a510106c
kion <kion@dashgl.com>
2017-10-26 13:10:11 +0000

    trying not to lose progress

---
+++ 07_Uniform_Matrix/shader/fragment.glsl
@@ -0,0 +1,11 @@
+#version 130
+
+varying vec2 texcoord;
+uniform sampler2D mytexture;
+
+void main(void) {
+	
+	vec2 flipped_texcoord = vec2(texcoord.x, 1.0 - texcoord.y);
+	gl_FragColor = texture2D(mytexture, flipped_texcoord);
+
+}
---
+++ 07_Uniform_Matrix/shader/vertex.glsl
@@ -0,0 +1,12 @@
+#version 130
+
+uniform mat4 orthograph;
+uniform mat4 mvp;
+attribute vec2 coord2d;
+attribute vec2 uv_coord;
+varying vec2 texcoord;
+
+void main (void) {
+	gl_Position = orthograph * vec4(coord2d, 0.0, 1.0);
+	texcoord = uv_coord;
+}
